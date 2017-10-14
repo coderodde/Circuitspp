b15ba5a95dd289a379792653c28a8bebc0125550
coderodde <rodion.efremov@cs.helsinki.fi>
2017-10-14 11:01:16 +0300

    Circuit passes all the tests.

--- main.cpp
+++ main.cpp
@@ -137,12 +137,77 @@ void test2() {
     }
 }
 
+void testSubcircuit() {
+    Circuit subcircuit {"mySubcircuit", 2, 1};
+    
+    subcircuit.addAndGate("and");
+    subcircuit.addNotGate("not1");
+    subcircuit.addNotGate("not2");
+    
+    subcircuit.connectTo("inputPin0", "not1");
+    subcircuit.connectTo("inputPin1", "not2");
+    subcircuit.connectToFirstInputPin("not1", "and");
+    subcircuit.connectToSecondInputPin("not2", "and");
+    subcircuit.connectTo("and", "outputPin0");
+    
+    Circuit circuit {"myCircuit", 2, 1};
+    circuit.addCircuit(&subcircuit);
+    circuit.addNotGate("not");
+    
+    circuit.connectTo("inputPin0", "mySubcircuit.inputPin0");
+    circuit.connectTo("inputPin1", "mySubcircuit.inputPin1");
+    circuit.connectTo("mySubcircuit.outputPin0", "not");
+    circuit.connectTo("not", "outputPin0");
+    
+    for (bool bit0 : { false, true }) {
+        for (bool bit1 : { false, true }) {
+            std::vector<bool> input = { bit0, bit1 };
+            std::vector<bool> output = circuit.doCycle(input);
+            bool expected = !(!bit0 && !bit1);
+            ASSERT(output.size() == 1);
+            ASSERT(output[0] == expected);
+        }
+    }
+}
+
+void testCopyConstructor() {
+    Circuit c1{"c", 2, 2};
+    
+    c1.addAndGate("and");
+    c1.addOrGate("or");
+    
+    c1.connectToFirstInputPin("inputPin0", "and");
+    c1.connectToSecondInputPin("inputPin1", "and");
+    c1.connectToFirstInputPin("inputPin0", "or");
+    c1.connectToSecondInputPin("inputPin1", "or");
+    c1.connectTo("and", "outputPin0");
+    c1.connectTo("or", "outputPin1");
+    
+    Circuit c2{c1, "c2"};
+    
+    for (bool bit0 : { false, true }) {
+        for (bool bit1 : { false, true }) {
+            std::vector<bool> input = { bit0, bit1 };
+            std::vector<bool> output1 = c1.doCycle(input);
+            std::vector<bool> output2 = c2.doCycle(input);
+            ASSERT(output1.size() == 2);
+            ASSERT(output2.size() == 2);
+            
+            for (int i = 0; i < 2; ++i) {
+                ASSERT(output1[i] == output2[i]);
+            }
+        }
+    }
+}
+
 void test() {
     testFindsForwardCycle();
     testFindsBackwardCycle();
     testCannotConnectToOccupiedInput();
     test1();
     test2();
+    testSubcircuit();
+    testCopyConstructor();
 }
 
 int main() {
